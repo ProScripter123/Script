0870e89ecd3977849d3703d25e8acab1c573ba21
ProScripter123 <121627858+ProScripter123@users.noreply.github.com>
2023-10-08 20:45:13 +0700

    Pull A Sword

---
+++ AutoClaimGift.lua
@@ -0,0 +1,38 @@
+while task.wait() do
+local A_1 = "Reward1"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward2"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward3"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward4"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward5"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward6"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward7"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward8"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward9"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward10"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward11"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+local A_1 = "Reward12"
+local Event = game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift
+Event:FireServer(A_1)
+end
